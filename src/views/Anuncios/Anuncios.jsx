029449b14c6471cdbbb8a9a2ee56d6f278e26158
Nayrb29 <bryan201429@gmail.com>
2023-11-01 21:24:13 -0500

    Advertise

--- src/views/Anuncios/Anuncios.jsx
+++ src/views/Anuncios/Anuncios.jsx
@@ -1,9 +1,10 @@
 import './Anuncios.css'
+import advertiseIcon from '../../assets/advertiseIcon.png'
 
 export default function Anuncios(){
     return(
         <div className="AnunciosContainer">
-
+            <div className='anuncieContainer'>Anuncie aqui! <img src={advertiseIcon}></img></div>
         </div>
     )
 }
